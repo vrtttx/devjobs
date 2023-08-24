e25d434c74b1d7e7e6900658cd1432185a681e57
ynncstslv <ynncstslv@gmail.com>
2023-08-24 10:00:44 -0300

    feat: all actions logic

---
+++ app/actions/getListings.ts
@@ -0,0 +1,43 @@
+import prisma from '@/app/libs/prismadb';
+
+export interface IListingsParams {
+	userId?: string;
+	category?: string;
+	locationValue?: string;
+}
+
+export default async function getListings(params: IListingsParams) {
+	try {
+		const { userId, category, locationValue } = params;
+
+		let query: any = {};
+
+		if (userId) {
+			query.userId = userId;
+		}
+
+		if (category) {
+			query.category = category;
+		}
+
+		if (locationValue) {
+			query.locationValue = locationValue;
+		}
+
+		const listings = await prisma.listing.findMany({
+			where: query,
+			orderBy: {
+				createdAt: 'desc',
+			},
+		});
+
+		const safeListings = listings.map((listing) => ({
+			...listing,
+			createdAt: listing.createdAt.toISOString(),
+		}));
+
+		return safeListings;
+	} catch (error: any) {
+		throw new Error(error);
+	}
+}

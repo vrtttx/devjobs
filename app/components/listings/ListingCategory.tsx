76ff39a5e311951748d1287208d88ca56df362bc
ynncstslv <ynncstslv@gmail.com>
2023-08-24 15:41:51 -0300

    fix: listing page styling changes

--- app/components/listings/ListingCategory.tsx
+++ app/components/listings/ListingCategory.tsx
@@ -11,9 +11,10 @@ interface ListingCategoryProps {
 const ListingCategory: FC<ListingCategoryProps> = ({ icon: Icon, label }) => {
 	return (
 		<div className="flex flex-col gap-6">
-			<div className="flex flex-row items-center gap-3">
-				<Icon size={26} className="text-blue-600" />
-				<div className="font-semibold text-blue-600">{label}</div>
+			<div className="flex flex-row items-center gap-2">
+				<div className="text-neutral-600">Technology: </div>
+				<Icon size={22} className="text-blue-600" />
+				<div className="font-light text-blue-600">{label}</div>
 			</div>
 		</div>
 	);
--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -11,12 +11,14 @@ import Image from 'next/image';
 
 import ListingCategory from './ListingCategory';
 import HeartButton from '@/app/components/HeartButton';
+import Link from 'next/link';
 
 interface ListingHeadProps {
 	currentUser?: SafeUser | null;
 	id: string;
 	imageSrc: string;
 	title: string;
+	description: string;
 	company: string;
 	employeeCount: number;
 	locationValue: string;
@@ -34,6 +36,7 @@ const ListingHead: FC<ListingHeadProps> = ({
 	id,
 	imageSrc,
 	title,
+	description,
 	company,
 	employeeCount,
 	locationValue,
@@ -50,88 +53,77 @@ const ListingHead: FC<ListingHeadProps> = ({
 
 	return (
 		<>
-			<div className="w-full h-full">
+			<div className="w-full h-full pt-[7rem] pb-6">
 				<div className="flex flex-col items-center justify-evenly gap-8 md:flex-row lg:gap-6">
-					<div className="aspect-square w-[300px] relative rounded-xl overflow-hidden md:w-[320px] md:h-[45vh] lg:w-[500px] lg:h-full">
+					<div className="aspect-square w-[300px] relative rounded-xl overflow-hidden md:w-[320px] md:h-[45vh] lg:w-[430px] lg:h-full">
 						<Image
 							src={imageSrc}
-							alt="Image"
+							alt="Company Logo"
 							className="w-full object-cover"
 							fill
 						/>
 					</div>
-					<div className="flex flex-col gap-6">
-						<div className="font-bold text-4xl">{title}</div>
-						<div className="flex flex-col gap-2">
-							<div className="font-semibold text-xl lg:text-2xl text-blue-950">
-								{company}
-							</div>
-							<div className="flex flex-row items-center gap-6 font-medium text-sm text-neutral-500 lg:text-md">
-								<div>
-									{location?.label}, {location?.region}
-								</div>
-								<div>{employeeCount} Employees</div>
-							</div>
+					<div className="w-full flex flex-col gap-3">
+						<div className="font-light text-xl text-neutral-600 lg:text-3xl">
+							{company}
 						</div>
+						<div className="font-bold text-2xl lg:text-4xl">{title}</div>
+						<div className="font-light text-base text-neutral-500 lg:text-xl">
+							{location?.label}, {location?.region}
+						</div>
+						{category && (
+							<ListingCategory icon={category.icon} label={category.label} />
+						)}
 						<hr />
-						<div className="w-full p-6 rounded-xl bg-neutral-100">
-							<div className="mb-4 font-bold text-lg">Job Info:</div>
-							<div className="w-full grid grid-cols-2 items-center justify-between gap-4 font-semibold text-sm text-blue-600 lg:text-base">
-								{category && (
-									<ListingCategory
-										icon={category.icon}
-										label={category.label}
-									/>
-								)}
+						<div className="flex flex-col space-y-2 p-3 font-light text-sm text-blue-700 rounded-md border-gray-200 bg-white shadow-md lg:text-base">
+							<div className="flex items-center gap-8">
 								<div>
-									<span className="text-xs text-neutral-500 lg:text-sm">
-										Salary:{' '}
-									</span>
-									$ {salary}{' '}
-									<span className="text-xs text-neutral-500 lg:text-sm">
-										/ y
-									</span>
-								</div>
-								<div>
-									<span className="text-xs text-neutral-500 lg:text-sm">
-										XP Level:{' '}
-									</span>
-									{xpLevelValue}
-								</div>
-								<div>
-									<span className="text-xs text-neutral-500 lg:text-sm">
-										XP:{' '}
-									</span>
-									{xpCount}{' '}
-									<span className="text-xs text-neutral-500 lg:text-sm">
-										Years
-									</span>
-								</div>
-								<div>
-									<span className="text-xs text-neutral-500 lg:pr-1.5 lg:text-sm">
-										Job Type:{' '}
-									</span>
+									<span className="text-neutral-600">Job Type:</span>{' '}
 									{jobTypeValue}
 								</div>
 								<div>
-									<span className="text-xs text-neutral-500 lg:pr-1.5 lg:text-sm">
-										Visa Sponsorship:{' '}
-									</span>
+									<span className="text-neutral-600">Salary: $</span> {salary} /
+									Year
+								</div>
+							</div>
+							<div className="flex items-center gap-8">
+								<div>
+									<span className="text-neutral-600">Experience Level:</span>{' '}
+									{xpLevelValue}
+								</div>
+								<div>
+									<span className="text-neutral-600">Years of Experience:</span>{' '}
+									{xpCount}
+								</div>
+							</div>
+							<div className="flex items-center gap-8">
+								<div>
+									<span className="text-neutral-600">Company Size:</span>{' '}
+									{employeeCount} Employees
+								</div>
+								<div>
+									<span className="text-neutral-600">Visa Sponsorship:</span>{' '}
 									{visaValue}
 								</div>
 							</div>
 						</div>
-						<hr />
-						<div className="flex flex-row items-center justify-between gap-4">
-							<button
-								className="w-[250px] rounded-full transition hover:opacity-80 disabled:opacity-70 disabled:cursor-not-allowed py-3 font-semibold text-md border-2 text-white border-blue-600 bg-blue-600 lg:w-[300px]"
-								onClick={() => window.open(jobApply, '_blank')}
-							>
-								Apply
-							</button>
-							<HeartButton currentUser={currentUser} listingId={id} />
-						</div>
 					</div>
+				</div>
+				<div className="flex flex-col gap-3 mx-auto my-8">
+					<div className="font-light text-lg text-gray-500 lg:text-xl">
+						Job Description
+					</div>
+					<hr />
+					<div className="font-base text-base lg:text-lg">{description}</div>
+					<hr />
+				</div>
+				<div className="w-full flex flex-row items-center justify-between gap-8">
+					<button className="w-full rounded-md transition hover:opacity-80 disabled:opacity-70 disabled:cursor-not-allowed py-3 font-semibold text-md border-2 text-white border-blue-600 bg-blue-600">
+						<Link href={jobApply} target="_blank">
+							Apply
+						</Link>
+					</button>
+					<HeartButton currentUser={currentUser} listingId={id} />
 				</div>
 			</div>
 		</>
